59cd43b23178be21949b89eb8c0abb9e8a83ece5
xlX-Xlx <150480030+xlX-Xlx@users.noreply.github.com>
2024-05-11 00:52:40 +0300

    Add files via upload

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import os
 import sqlite3
 from flask import Flask, render_template, request, redirect, url_for, flash, session, jsonify
+from generate import alg
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
@@ -14,14 +15,18 @@ cursor.execute('''CREATE TABLE IF NOT EXISTS users (
                   id INTEGER PRIMARY KEY AUTOINCREMENT,
                   username TEXT UNIQUE NOT NULL,
                   email TEXT UNIQUE NOT NULL,
-                  password TEXT NOT NULL)''')
+                  password TEXT NOT NULL,
+                  rating INTEGER DEFAULT 0)
+                  ''')
 
 conn.commit()
 conn.close()
 
+
 @app.route('/')
 def index():
     return render_template('login.html')
+
 
 @app.route('/upload_avatar', methods=['POST'])
 def upload_avatar():
@@ -100,6 +105,7 @@ def admin_panel():
         flash("Доступ к админ панели запрещен.", 'error')
         return redirect(url_for('index'))
 
+
 @app.route('/profile')
 def profile():
     if 'username' in session:
@@ -120,6 +126,7 @@ def profile():
         flash("Пожалуйста, войдите в систему, чтобы получить доступ к профилю.", 'error')
         return redirect(url_for('index'))
 
+
 @app.route('/person_panel')
 def person_panel():
     if 'username' in session:
@@ -132,10 +139,41 @@ def person_panel():
 @app.route('/test_easy')
 def test_easy():
     if 'username' in session:
-        return render_template('test_easy.html')
+        if 'attempts' not in session:
+            session['attempts'] = 3  # Устанавливаем количество попыток
+        example = alg.Generate.simple_examples(self=None)
+        session['example'] = example
+        return render_template('test_easy.html', example=example, attempts=session['attempts'])
     else:
         flash("Пожалуйста, войдите в систему, чтобы получить доступ к тесту", 'error')
         return redirect(url_for('index'))
+
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    if request.method == 'POST':
+        example = session.get('example')
+        answer = request.form['answer']
+        correct_answer = example[1]
+        if int(answer) == int(correct_answer):
+            print("Получен правильный ответ:", answer)
+            # Получаем имя пользователя из сессии
+            username = session.get('username')
+            # Увеличиваем рейтинг пользователя на 1
+            conn = sqlite3.connect('users.db')
+            cursor = conn.cursor()
+            cursor.execute("UPDATE users SET rating = rating + 1 WHERE username = ?", (username,))
+            conn.commit()
+            conn.close()
+            print("Рейтинг пользователя увеличен.")
+            return "Ответ верный!"
+        else:
+            session['attempts'] -= 1
+            print("Получен неправильный ответ:", answer)
+            print("Осталось попыток:", session['attempts'])
+            return redirect(url_for('test_easy'))
+    return "Что-то пошло не так!"
+
 
 @app.route('/test_normal')
 def test_normal():
@@ -145,6 +183,7 @@ def test_normal():
         flash("Пожалуйста, войдите в систему, чтобы получить доступ к тесту", 'error')
         return redirect(url_for('index'))
 
+
 @app.route('/test_hard')
 def test_hard():
     if 'username' in session:
@@ -153,6 +192,7 @@ def test_hard():
         flash("Пожалуйста, войдите в систему, чтобы получить доступ к тесту", 'error')
         return redirect(url_for('index'))
 
+
 @app.route('/test_very_hard')
 def test_very_hard():
     if 'username' in session:
@@ -160,6 +200,7 @@ def test_very_hard():
     else:
         flash("Пожалуйста, войдите в систему, чтобы получить доступ к тесту", 'error')
         return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
