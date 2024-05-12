7f480d99448dd6ff7c14c3c501a3f1accd6f71cb
xlX-Xlx <150480030+xlX-Xlx@users.noreply.github.com>
2024-05-12 17:10:59 +0300

    Add files via upload

--- server.py
+++ server.py
@@ -4,11 +4,20 @@ from flask import Flask, render_template, request, redirect, url_for, flash, ses
 from algGenerate import Simple, Normal
 import random
 import json
+import requests
+import secrets
+import string
 import html
 
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
+
+CLIENT_ID = '623a52b24eba4a6888d3b87d0b2904e2' 
+CLIENT_SECRET = '453cfc6b79fe4b2084d803061731ab12'
+TOKEN_URL = 'https://oauth.yandex.ru/token'
+AUTH_URL = 'https://oauth.yandex.ru/authorize'
+REDIRECT_URI = 'http://127.0.0.1:8000/index'
 
 admin_username = 'admin'
 admin_password = 'qwerty'
@@ -105,6 +114,8 @@ def login_post():
 
         conn.close()
 
+        print(user)
+
         if user and user[1] == password:
             session['username'] = username
             flash(f"Добро пожаловать, {username}!", 'success')
@@ -137,6 +148,59 @@ def register():
             return redirect(url_for('index'))
 
     return render_template('register.html')
+
+
+@app.route('/YanRedir') 
+def YanRedir():
+    auth_url = f'{AUTH_URL}?response_type=code&client_id={CLIENT_ID}&redirect_uri={REDIRECT_URI}'
+    return redirect(auth_url)
+
+
+@app.route('/index') 
+def callback():
+    auth_code = request.args.get('code')
+
+    token_data = {
+        'grant_type': 'authorization_code',
+        'code': auth_code,
+        'client_id': CLIENT_ID,
+        'client_secret': CLIENT_SECRET,
+        'redirect_uri': REDIRECT_URI
+    }
+    response = requests.post(TOKEN_URL, data=token_data)
+
+    access_token = response.json().get('access_token')
+
+    headers = {
+        'Authorization': f'OAuth {access_token}'
+    }
+
+    data = requests.get("https://login.yandex.ru/info", headers=headers).json()
+
+    email = data["default_email"]
+    name = data["first_name"]
+    alphabet = string.ascii_letters + string.digits
+    password = ''.join(secrets.choice(alphabet) for i in range(20))
+
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT email, password FROM users WHERE email = ?", (email,))
+    email_user = cursor.fetchone()
+
+    if email_user is None:
+        cursor.execute("INSERT INTO users (email, password, username) VALUES (?, ?, ?)", (email, password, name))
+        conn.commit()
+        session['username'] = name
+        flash(f"Добро пожаловать, {name}!", 'success')
+        return redirect(url_for('person_panel'))
+    else:
+        cursor.execute("SELECT username FROM users WHERE email = ?", (email,))
+        username = cursor.fetchone()[0]
+        session['username'] = username  # Set session to username
+        flash(f"Добро пожаловать, {username}!", 'success')
+        return redirect(url_for('person_panel'))
+
+    conn.close()
 
 
 @app.route('/admin_panel')
@@ -320,4 +384,4 @@ def test_very_hard():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="127.0.0.1", port=8000, debug=True)
